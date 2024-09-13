9e887bf9bd44d5becfcbef97e14cd39eea35e389
Nihal Chehade <nihalche@icloud.com>
2024-09-12 23:16:33 -0700

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import JokeList from "./JokeList";
+
+const App = () => {
+  return (
+    <div className='App'>
+        <JokeList />
+    </div>
+  )
+}
+
+export default App
